be3826a1680eb175de30516f4d6859797b885b44
Kethireddy Ajay kumar Reddy <ajaykumarreddy7474@gmail.com>
2024-05-13 09:16:03 +0530

    2nd

--- app/components/SideNavbar.jsx
+++ app/components/SideNavbar.jsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 import T1 from '../../public/logo.png';
 const SideNavbar = () => {
   return (
-    <div className="w-[90px] flex flex-col  lg:w-72 bg-blue-700 lg:h-screen">
+    <div className="max-[420px]:w-[20%]  w-[90px] flex flex-col  lg:w-72 bg-blue-700 lg:h-screen">
         <div className="flex flex-col pt-5 ml-3">
 <div>
 
-        <Link href='/' className="flex items-center gap-2"> <Image src={T1} alt='pic'></Image> <span className="hidden lg:block font-black text-xl">WORKFLOW</span></Link>
+        <Link href='/' className=" max-[420px]:ml-[10px] flex items-center gap-2"> <Image src={T1} alt='pic'></Image> <span className="hidden lg:block font-black text-xl">WORKFLOW</span></Link>
 </div>
 
                 <div className="flex flex-col pt-10 px-2 " >
